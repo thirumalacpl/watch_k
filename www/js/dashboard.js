de50768e35cdb9aa80b43dfb09784fa5bc59abba
kalyan <kalyan@staging.eimpressive.com>
2016-02-03 16:06:08 +0530

    watch_k 72564

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -31,7 +31,47 @@ if(regionArray_array == null){
   return false;
 }
 
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "728597627187"
+            },
+            "ios": {"alert": "true", "badge": "true", "sound": "true"}, 
+            "windows": {} 
+        });
 
+        
+        push.on('registration', function(data) {
+            console.log("registration event");
+            document.getElementById("regId").innerHTML = data.registrationId;
+            console.log(JSON.stringify(data));
+        });
+
+    push.on('registration', function(data) {
+        console.log("registration event");
+        var gcm_regid = data.registrationId;
+        alert(gcm_regid+'numb');
+
+    });
+        push.on('error', function(e) {
+            console.log("push error");
+        });
+    }
+    };
+
+    app.initialize();
 //alert(user_id);
 
 regionArray_array =  JSON.parse(sessionStorage.getItem("regionArray"));
